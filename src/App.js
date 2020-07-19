e806b69a0aee88857f5af3e4d2b8a5e0059e04a8
amitaduangta25 <amitaduangta@gmail.com>
2020-07-19 10:04:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <hi> ยินดีต้อนรับ </hi>
+    <p>
+    </p>
+    </div>
+  );
+}
+
+export default App;
